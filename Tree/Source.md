8e74872a00f340a4fa88854af2e367e59d4c26ce
Naufal <nmuntaaza.wh@gmail.com>
2019-12-26 17:53:11 +0700

    add tree function
    -- numberOfNodes
    -- countNode
    -- countLeafNode

---
+++ Tree/Source.md
@@ -0,0 +1,3 @@
+https://www.cs.cmu.edu/~adamchik/15-121/lectures/Trees/trees.html
+https://www.gatevidyalay.com/tree-data-structure-tree-terminology/
+https://www.geeksforgeeks.org/binary-tree-data-structure/
--- Tree/Tree.js
+++ Tree/Tree.js
@@ -1,3 +1,16 @@
+/*
+  A BST is a binary tree where nodes are ordered in the following way:
+    1. Each node contains one key (also known as data)
+    2. The keys in the left subtree are less then the key in its parent node, in short L < P
+    3. The keys in the right subtree are greater the key in its parent node, in short P < R
+    4. Duplicate keys are not allowed
+
+  Depth: Is the number of edges from the tree's root node to the node.
+    A root node will have a depth of 0
+  Height: Is the number of edges on the longest path from the node to a leaf
+    A leaf node will have a height of 0
+*/
+
 const arr = [1, 2, 5, 3, 5, 3, 9, 7, 1, 4];
 
 class Node {
@@ -21,7 +34,8 @@ class BinaryTree {
   }
 
   _insertNode(node, newNode) {
-    if (newNode.data <= node.data) {
+    if (newNode.data == node.data) return;
+    if (newNode.data < node.data) {
       node.left === null
         ? node.left = newNode
         : this._insertNode(node.left, newNode);
@@ -51,5 +65,39 @@ class BinaryTree {
     this.inOrder(node.left);
     console.log(node.data);
     this.inOrder(node.right);
+  }
+
+  countNode(node = this.root) {
+    if (this.root == null) return 0; // Immediate return. Close condition
+    let count = 1;
+    if (node.left != null) count += this.countNode(node.left);
+    if (node.right != null) count += this.countNode(node.right);
+    return count;
+  }
+
+  countLeafNode(node = this.root) {
+    if (this.root == null) return 0;
+    let count = 0;
+    if (node.left == null && node.right == null) count = 1;
+    if (node.left != null) count += this.countLeafNode(node.left);
+    if (node.right != null) count += this.countLeafNode(node.right);
+    return count;
+  }
+
+  depthOfNode(data) {
+    let curNode = this.root;
+    let depth = 0;
+    while (curNode != null) {
+      if (curNode.data == data) return depth;
+      data < curNode.data
+        ? curNode = curNode.left
+        : curNode = curNode.right;
+      depth++;
+    }
+    return -1;
+  }
+
+  numberOfEdges() {
+    return this.numberOfNode() - 1;
   }
 }
